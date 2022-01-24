4ccd312f07756bbc9fd3db863d2bb402b498e7c3
jack farrell <jack.farrell82@mail.dcu.ie>
2022-01-24 10:42:10 +0000

    added urls and basic models to the project

--- backend/jackshop/admin.py
+++ backend/jackshop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(APIUser)
+admin.site.register(Product)
+admin.site.register(Basket)
+admin.site.register(BasketItems)
+admin.site.register(Order)
--- backend/jackshop/views.py
+++ backend/jackshop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    # get the request in
+    # do some logic with models from the databasse
+    # return some webpage to the user
+    dic = {'name':'Jack', 'Title':'Mr'}
+    names = ['John', 'Paul', 'George', 'Ringo']
+    return render(request, "index.html", {'names': names})
